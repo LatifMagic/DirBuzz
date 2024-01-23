6afefb22f9071dd3496756d17f35eaf0137ad364
AbdelatifBen <Abdelatifbensahoun@gmail.com>
2024-01-23 16:40:32 +0100

    first commit

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,85 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { IContextType, IUser } from "@/types";
+import { getCurrentUser } from "@/lib/appwrite/api";
+import { useNavigate } from "react-router-dom";
+
+export const INITIAL_USER = {
+  id: "",
+  name: "",
+  username: "",
+  email: "",
+  imageUrl: "",
+  bio: "",
+};
+
+// We doing all this to know  wether we have loged in user at all times
+
+const INITIAL_STATE = {
+  user: INITIAL_USER,
+  isLoading: false,
+  isAuthenticated: false,
+  setUser: () => {},
+  setIsAuthenticated: () => {},
+  checkAuthUser: async () => false as boolean,
+};
+
+const Authcontext = createContext<IContextType>(INITIAL_STATE);
+
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<IUser>(INITIAL_USER);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const navigate = useNavigate();
+
+  const checkAuthUser = async () => {
+    try {
+      const currentAccount = await getCurrentUser();
+
+      if (currentAccount) {
+        setUser({
+          id: currentAccount.$id,
+          name: currentAccount.name,
+          username: currentAccount.username,
+          email: currentAccount.email,
+          imageUrl: currentAccount.imageUrl,
+          bio: currentAccount.bio,
+        });
+
+        setIsAuthenticated(true);
+        return true;
+      }
+      return false;
+    } catch (error) {
+      console.log(error);
+      return false;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      localStorage.getItem("cookieFallback") === "[]" ||
+      localStorage.getItem("cookieFallback") === null
+    )
+      navigate("/sign-in");
+
+    checkAuthUser();
+  }, []);
+
+  const value = {
+    user,
+    setUser,
+    isLoading,
+    isAuthenticated,
+    setIsAuthenticated,
+    checkAuthUser,
+  };
+
+  return <Authcontext.Provider value={value}>{children}</Authcontext.Provider>;
+};
+
+export default AuthProvider;
+
+export const useUserContext = () => useContext(Authcontext);
